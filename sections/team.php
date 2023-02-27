a83dcb91fd2c4c3f8a4cdab92ef1ab70f7437107
Ruslan <ruslan231984@yandex.ru>
2023-02-27 16:42:42 +0300

    added image

--- sections/team.php
+++ sections/team.php
@@ -7,51 +7,49 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-12 col-sm">
-                <div class="card card_first">
-                    <div class="card-photo">
-                        <img src="/media/img/team/artur.jpg" alt="Артур Нурмухаметов">
-                    </div>
-                    <div class="card-name"><?php echo $text['team']['n1'][$ln]?></div>
-                    <div class="card-pos"><?php echo $text['team']['d1'][$ln]?></div>
-                    <div class="card-info"><?php echo $text['team']['o1'][$ln]?></div>
-                    <div class="card-info"></div>
-                </div>
-            </div>
-            <div class="col-6 col-sm">
+            <div class="col-md-3 mb-3">
                 <div class="card">
                     <div class="card-photo">
-                        <img src="/media/img/team/fb.jpg" alt="Фархад Бикмуратов">
-                    </div>
-                    <div class="card-name"><?php echo $text['team']['n2'][$ln]?></div>
-                    <div class="card-pos"><?php echo $text['team']['d2'][$ln]?></div>
-                    <div class="card-info"><?php echo $text['team']['o2'][$ln]?></div>
-                </div>
-            </div>
-            <div class="col-6 col-sm">
-                <div class="card">
-                    <div class="card-photo">
-                        <img src="/media/img/team/rh.jpg" alt="Руслан Халиулин">
-                    </div>
-                    <div class="card-name"><?php echo $text['team']['n3'][$ln]?></div>
-                    <div class="card-pos"><?php echo $text['team']['d3'][$ln]?></div>
-                    <div class="card-info"><?php echo $text['team']['o3'][$ln]?></div>
-                </div>
-            </div>
-            <div class="col-6 col-sm">
-                <div class="card">
-                    <div class="card-photo">
-                        <img src="/media/img/team/azat.jpg" alt="Азат Oтепбай">
+                        <span class="imageBlock">
+                            <img src="/media/img/team/azat.jpg" alt="<?php echo $text['team']['n4'][$ln]?>">
+                        </span>
                     </div>
                     <div class="card-name"><?php echo $text['team']['n4'][$ln]?></div>
                     <div class="card-pos"><?php echo $text['team']['d4'][$ln]?></div>
                     <div class="card-info"><?php echo $text['team']['o4'][$ln]?></div>
                 </div>
             </div>
-            <div class="col-6 col-sm">
+            <div class="col-md-3 mb-3">
                 <div class="card">
                     <div class="card-photo">
-                        <img src="/media/img/team/idris.jpg" alt="Киёмов Идрисджон Жумакулович">
+                        <span class="imageBlock">
+                            <img src="/media/img/team/fb.jpg" alt="<?php echo $text['team']['n2'][$ln]?>">
+                        </span>
+                    </div>
+                    <div class="card-name"><?php echo $text['team']['n2'][$ln]?></div>
+                    <div class="card-pos"><?php echo $text['team']['d2'][$ln]?></div>
+                    <div class="card-info"><?php echo $text['team']['o2'][$ln]?></div>
+                </div>
+            </div>
+            <div class="col-md-3 mb-3">
+                <div class="card">
+                    <div class="card-photo">
+                        <span class="imageBlock">
+                            <img src="/media/img/team/ah.jpg" alt="<?php echo $text['team']['n6'][$ln]?>">
+                        </span>
+                    </div>
+                    <div class="card-name"><?php echo $text['team']['n6'][$ln]?></div>
+                    <div class="card-pos"><?php echo $text['team']['d6'][$ln]?></div>
+                    <div class="card-info"><?php echo $text['team']['o6'][$ln]?></div>
+                </div>
+            </div>
+
+            <div class="col-md-3 mb-3">
+                <div class="card">
+                    <div class="card-photo">
+                        <span class="imageBlock">
+                            <img src="/media/img/team/idris.jpg" alt="<?php echo $text['team']['n5'][$ln]?>">
+                        </span>
                     </div>
                     <div class="card-name"><?php echo $text['team']['n5'][$ln]?></div>
                     <div class="card-pos"><?php echo $text['team']['d5'][$ln]?></div>
