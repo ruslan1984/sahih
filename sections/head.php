0f2f7cbe773f9ec70f7bd8887c78b87206cb4e89
Ruslan <ruslan231984@yandex.ru>
2023-07-31 02:30:22 +0300

    preload

--- sections/head.php
+++ sections/head.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <link rel="shortcut icon" href="<?php echo $domain?>/favicon.ico"  type="image/svg+xml" />
     <link href="https://plus.google.com/109784895098888983084" rel="author" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    <link preload href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
