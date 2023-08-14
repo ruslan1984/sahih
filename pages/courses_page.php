01497f56b4de1b026e8fa8ea72aae00e0e0028c3
Ruslan <ruslan231984@yandex.ru>
2023-08-14 10:20:10 +0300

    g

--- pages/courses_page.php
+++ pages/courses_page.php
@@ -13,10 +13,13 @@
 
 </body>
 <script>
+
+
     (async ()=>{
         const Authorization="Bearer eyJhbGciOiJIUzI1NiJ9.eyJsb2dpbiI6InJ1c2xhbjIzMTk4NEB5YW5kZXgucnUiLCJuYW1lIjoi0KDRg9GB0LvQsNC9IiwiZXhwIjoyMDA3Mjc5MjA4fQ.evmBWvjAWRQ-Ue2apgw0qtow51aYKbVTPi9U9kxUuVk";
         const address= "https://arm-test.sahihinvest.ru";
         const url=address+"/api/courses/all";
+        // const url ="https://garab.ru/api/grammar";
         const response = await fetch(url,{
             method: "GET",
             headers: {
