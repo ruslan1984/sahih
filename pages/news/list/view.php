484bae7b5dae3fce215a97ba412bd948e71879bb
Ruslan <ruslan231984@yandex.ru>
2024-09-08 17:56:42 +0300

    news some changes

--- pages/news/list/view.php
+++ pages/news/list/view.php
@@ -1,14 +1,20 @@
 <div class="container page">
     <h1>Новости</h1>
     <?php foreach ($newsList as $key=> $item): ?>
-    <a class="newsListBlock" href=<?php echo "/news/".$key?>>
+    <div class="newsListBlock">
         <h4><?php echo $item['title']?></h4>
         <div class="newsListTextBlock">
             <img class="isImg" src=<?php echo $item['img']?> alt="">
-            <div class="text">
-                <?php echo mb_substr($item['text'], 0, 350)?>...
+            <div>
+                <div class="text">
+                    <?php echo mb_substr($item['text'], 0, 230)?>...
+                </div>
+                <div class="newsBtn">
+                    <a href=<?php echo "/news/".$key?> class="btn btn--main">Узнать подробнее</a>
+                </div>
             </div>
         </div>
-    </a>
+
+    </div>
     <?php endforeach?>
 </div>
