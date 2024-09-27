6650b41467e0dd80f5f024d51a3280cc6740f010
Ruslan <ruslan231984@yandex.ru>
2024-09-27 22:09:26 +0300

    red

--- pages/to_mobile/index.php
+++ pages/to_mobile/index.php
@@ -18,13 +18,14 @@ $Android = stripos($_SERVER['HTTP_USER_AGENT'],"Android");
 $webOS   = stripos($_SERVER['HTTP_USER_AGENT'],"webOS");
 
 
-if( $iPod || $iPhone ){
-    header('Location: https://apps.apple.com/ru/app/sahih-invest/id1533653991');
-}else if($iPad){
-    header('Location: https://apps.apple.com/ru/app/sahih-invest/id1533653991');
-}else if($Android){
-    header('Location: https://play.google.com/store/apps/details?id=com.sahih.invest&hl=en&gl=US');
-}
+// if( $iPod || $iPhone ){
+//     header('Location: https://apps.apple.com/ru/app/sahih-invest/id1533653991');
+// }else if($iPad){
+//     header('Location: https://apps.apple.com/ru/app/sahih-invest/id1533653991');
+// }else if($Android){
+//     header('Location: https://play.google.com/store/apps/details?id=com.sahih.invest&hl=en&gl=US');
+// }
+
 // else if($webOS){
 //     header('Location: https://apps.apple.com/ru/app/sahih-invest/id1533653991');
 // }
